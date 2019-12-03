21ad10ccc70dc2e2cf06bf354c67eec21cd11577
Marcin Puc <mpuc@vmware.localdomain>
2019-12-03 16:46:50 +0100

    Finish solution for day3 part1

--- days/d03p1.py
+++ days/d03p1.py
@@ -4,49 +4,60 @@ from utils.parse import Parser
 class Grid:
     def __init__(self):
         self.__cells = set()
-        self.__intersections = []
 
     def wire(self, direction, start, length):
-        pass
+        {
+            'L': self.__left,
+            'R': self.__right,
+            'U': self.__up,
+            'D': self.__down
+        }[direction](start, length)
 
-    def reset_intersections(self):
-        self.__intersections = []
+    @property
+    def cells(self):
+        return self.__cells
 
     def __up(self, start, length):
         x_s, y_s = start
         for y in range(y_s + 1, y_s + length + 1):
             cell = x_s, y
-            if cell in self.__cells:
-                self.__intersections.append(cell)
-            else:
-                self.__cells.add(cell)
+            self.__cells.add(cell)
 
     def __down(self, start, length):
         x_s, y_s = start
         for y in range(y_s - 1, y_s - length - 1, -1):
             cell = x_s, y
-            if cell in self.__cells:
-                self.__intersections.append(cell)
-            else:
-                self.__cells.add(cell)
+            self.__cells.add(cell)
 
     def __left(self, start, length):
         x_s, y_s = start
         for x in range(x_s - 1, x_s - length - 1, -1):
             cell = x, y_s
-            if cell in self.__cells:
-                self.__intersections.append(cell)
-            else:
-                self.__cells.add(cell)
+            self.__cells.add(cell)
 
     def __right(self, start, length):
         x_s, y_s = start
         for x in range(x_s + 1, x_s + length + 1):
             cell = x, y_s
-            if cell in self.__cells:
-                self.__intersections.append(cell)
-            else:
-                self.__cells.add(cell)
+            self.__cells.add(cell)
+
+
+def move_pos(pos, d, l):
+    x, y = pos
+
+    if d == 'L':
+        return x - l, y
+    elif d == 'R':
+        return x + l, y
+    elif d == 'U':
+        return x, y + l
+    else:
+        return x, y - l
+
+
+def dist(pos):
+    x, y = pos
+    return abs(x) + abs(y)
 
 
 parser = Parser()
@@ -55,3 +66,21 @@ with parser.input as input:
     wire_a = [(path[:1], int(path[1:])) for path in line1.strip().split(',')]
     line2 = input.readline()
     wire_b = [(path[:1], int(path[1:])) for path in line2.strip().split(',')]
+
+grid_a = Grid()
+current_pos = (0, 0)
+for d, l in wire_a:
+    grid_a.wire(d, current_pos, l)
+    current_pos = move_pos(current_pos, d, l)
+
+grid_b = Grid()
+current_pos = (0, 0)
+for d, l in wire_b:
+    grid_b.wire(d, current_pos, l)
+    current_pos = move_pos(current_pos, d, l)
+
+intersections = grid_a.cells & grid_b.cells
+distances = map(dist, intersections)
+closest_dist = min(distances)
+
+print(closest_dist)
