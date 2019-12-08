9d495f8b87ee3cea700a07b9ffc9d588ecb5695f
Marcin Puc <mpuc@vmware.localdomain>
2019-12-08 13:29:33 +0100

    Remove unnecessary backslash in utils.parse

--- utils/parse.py
+++ utils/parse.py
@@ -1,10 +1,11 @@
 import sys
 from argparse import ArgumentParser, FileType
 
+
 class Parser:
     def __init__(self, desc=None):
         parser = ArgumentParser(description=desc)
-        parser.add_argument("infile", nargs="?", type=FileType('r'), default=sys.stdin, metavar="FILEPATH", \
+        parser.add_argument("infile", nargs="?", type=FileType('r'), default=sys.stdin, metavar="FILEPATH",
                             help="Input file path, default: STDIN")
         self.__args = parser.parse_args()
 
