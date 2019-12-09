a6d28c6462fb42db66cb343c43ad42d13d61d534
Marcin Puc <mpuc@vmware.localdomain>
2019-12-09 19:27:11 +0100

    Refactor solution for day2 part2

--- days/d02p2.py
+++ days/d02p2.py
@@ -3,18 +3,22 @@ from misc.intcode import Machine
 from itertools import product
 from copy import deepcopy
 
+
+def run_with_params(program, noun, verb):
+    program[1] = noun
+    program[2] = verb
+    computer = Machine(deepcopy(program), lambda: None)
+    computer.run()
+    return computer.read(0)
+
+
 parser = Parser("Day 2: 1202 Program Alarm - Part 2")
 with parser.input as input:
     line = input.readline()
     program = [int(el) for el in line.split(',')]
 
-for noun, verb in product(range(100), range(100)):
-    program[1] = noun
-    program[2] = verb
-    computer = Machine(deepcopy(program), lambda: None)
-    computer.run()
-    output = computer.read(0)
-    if output == 19690720:
-        solution = 100 * noun + verb
-        print(solution)
-        break
+filtered = ((n, v) for n, v in product(range(100), range(100)) if run_with_params(program, n, v) == 19690720)
+noun, verb = next(filtered)
+
+solution = 100 * noun + verb
+print(solution)
